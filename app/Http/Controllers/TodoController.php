96f0edd882fab5ad5f91d46a5612d0fc1825099b
unesaki <unesaki1610@gmail.com>
2022-06-21 17:08:41 +0900

    first-commit

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -10,21 +10,18 @@ class TodoController extends Controller
 {
     public function index() //トップページの表示
     {
-        $items = DB::select('select * from schedule'); //DB::select　がデータベースの中から取り出す処理
+        $items = DB::select('select * from todos'); //DB::select　がデータベースの中から取り出す処理
         //()の中はSQL分を記述することで値を取得できる
         return view('index', ['items' => $items]);
     }
-    public function add()
-    {
-        return view('add');
-    }
+
     public function create(Request $request)
     {
-        $param = [
-            'content' => $request->content,
-        ];
-        DB::insert('insert into schedule (content) values (:content)', $param);
-        return redirect('/');
+        Todo::create([
+            'content' => $request->content
+        ]);
+
+        return redirect()->route('todo.init');
     }
     
 }
--- database/migrations/2022_06_21_072702_create_todos_table.php
+++ database/migrations/2022_06_21_072702_create_todos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateScheduleTable extends Migration
+class CreateTodosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateScheduleTable extends Migration
      */
     public function up()
     {
-        Schema::create('schedule', function (Blueprint $table) {
+        Schema::create('todos', function (Blueprint $table) {
             $table->bigInteger('id', 20)->primary()->nullable(false);
             $table->string('content', 191)->nullable(false);
             $table->timestamp('created_at')->useCurrent()->nullable();
@@ -28,6 +28,6 @@ class CreateScheduleTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('schedule');
+        Schema::dropIfExists('todos');
     }
 }
